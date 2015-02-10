9bc53d4eb48abf288cb279a606f6fed6145b2841
i9maki2 <Mladen@Mladens-MacBook-Pro.local>
2015-02-10 19:57:41 +0100

    Dishes are now displaying in the planner.html page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(function() {
 
   var model = new DinnerModel(),
-        view = new DinnerView(model, {'dinnerView': $('#dinnerView')}),
+        view = new DinnerView(model, {'dinnerView': $('#dinnerView'), 
+        							  'selectDish': $('#selectDish'),        							  
+        							 }),
         controller = new DinnerController(model, view);
 });
--- js/view/dinnerView.js
+++ js/view/dinnerView.js
@@ -2,13 +2,53 @@
 var DinnerView = function (model, elements) {
 
 	this._model = model;
-    this._elements = elements;
+	this._elements = elements;
 	
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
-	this.numberOfGuests = this._elements.dinnerView.find("#numberOfGuests");
-	this.plusButton = this._elements.dinnerView.find("#plusGuest");
-	this.minusButton = this._elements.dinnerView.find("#minusGuest");
+
+	//Planner.html----------------------------------------------------
+	//----------------------------------------------------------------
+
+
+	//Left Navigational Bar (My Dinner)
+	//----------------------------------------------------------------
+	//Number Of Guests
+	this.numberOfGuests = $("#numberOfGuests");
+	this.numberOfGuests.val(this._model.numberOfGuests);
+
+	//Plus Minus Buttons
+	this.plusButton = $("#plusGuest");
+	this.minusButton = $("#minusGuest");
 	
-	this.numberOfGuests.html(this._model.numberOfGuests);
+
+	//Main Part (Select Dish)
+	//----------------------------------------------------------------
+	//Choosing a course (dropdown menu)
+	this.courseDropDown = $("#courses");
+
+	//Search Bar
+	this.searchBarInputText = $("searchBarInputText");
+	this.searchBarButton = $("searchBarButton");
+	
+
+	//Dishes (traversing Array to show each dish from the specific course)
+	//In this case it will show only main dish)
+	this.dishes = this._elements.selectDish.find("#dishes");
+
+	var dish = this._model.getAllDishes("main dish", "");
+
+	for (var i = 0; i < dish.length; i++) {		
+
+		var html = '<div class="col-xs-6 col-sm-3 placeholder"><img src="images/'
+		+ dish[i].image +'" alt="..." class="img-thumbnail" style="max-height:200px; max-width:200px;"><h4>'
+		+ dish[i].name +'</h4><span class="text-muted" style="font-size: 14px">'
+		+ dish[i].description +'</span></div>';
+
+		this.dishes.append(html);
+	};
+
+	//END OF Planner.html---------------------------------------------
+
+			
 }
