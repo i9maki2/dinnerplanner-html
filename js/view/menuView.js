feb89e95d9b49bb32314a4091ac7133f3ad95949
agentff6600 <ff6600@ff6600.local>
2015-02-23 22:26:21 +0100

    implemented listener in menuView / Menu has correct order and Sum

--- js/view/menuView.js
+++ js/view/menuView.js
@@ -11,6 +11,19 @@ var MenuView = function(model, elements)
 	this.backButtonClicked = new Event(this);
 	this.printButtonClicked = new Event(this);
 
+// attach MODEL listeners
+    this._model.guestAdded.attach(function () {
+		_this.dishes.html('');
+		//Load Course List On Page Load
+		_this.updateList();
+    });
+    this._model.guestRemoved.attach(function () {
+		_this.dishes.html('');
+		//Load Course List On Page Load
+    	_this.updateList();
+    });
+
+
 	//Buttons
 	this.backButton = this._elements.menu.find("#backButton");
 	this.printButton = this._elements.menu.find("#printButton");
@@ -38,19 +51,25 @@ var MenuView = function(model, elements)
 
 MenuView.prototype = {
 	updateList: function(){
-		var dish = this._model.getAllDishes("starter", "");
 
-		for (var i = 0; i < dish.length; i++) {
+		this.selectedDishes = this._elements.menu.find("#fullMenu");
 
-			var html = '<div class="col-xs-6 col-sm-3 placeholder"><h1 class="h5-type" style="text-transform:capitalize;">'
-			+ dish[i].type + '</h1><img src="images/'
-			+ dish[i].image +'" alt="..." class="img-thumbnail" style="max-height:200px; max-width:200px; margin:20px;"><h4>'
-			+ dish[i].name +'</h4><span class="text-muted" style="font-size: 14px">'
-			+ dish[i].description +'</span></div>';
+    	_.each(this._model.selectedDishes, function(dish) {
 
-			this.dishes.prepend(html);
-		};
+			var menu = '<div class="col-xs-6 col-sm-3 placeholder"><h1 class="h5-type" style="text-transform:capitalize;">'
+			+ dish.type + '</h1><img src="images/'
+			+ dish.image +'" alt="..." class="img-thumbnail" style="max-height:200px; max-width:200px; margin:20px;"><h4>'
+			+ dish.name +'</h4><span class="text-muted" style="font-size: 14px">'
+			+ dish.description +'</span></div>';
 
-	}
+			this.dishes.append(menu);
 
+		}, this);
+
+
+		var total = '<div class="col-xs-6 col-sm-3" style="border-left: 2px solid #ddd; text-align: center;"><h1 class="h5-type" style="text-transform:capitalize;">Total</h1><img src="images/sum.png" class="img-thumbnail" style="max-height:200px; max-width:200px;"><h1 style="text-decoration: underline;">' + this._model.getTotalMenuPrice() + ' SEK</h1></div>';
+
+    	this.dishes.append(total);
+
+	},
 }
